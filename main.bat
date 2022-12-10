7a0c3ae1cea782ff1a9fde17d5ffb4bcf414f1cf
similartobash <120057329+similartobash@users.noreply.github.com>
2022-12-10 22:37:43 +0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,3 +1,15 @@
 @echo off
-echo + > "%USERPROFILE%\Desktop\Yeni Metin Belgesi.txt"
+rem echo + > "%USERPROFILE%\Desktop\Yeni Metin Belgesi.txt"
+
+if exist "%USERPROFILE%\Desktop\Yeni Metin Belgesi.txt" (
+mkdir "%USERPROFILE%\Desktop\esc0"
+mkdir "%USERPROFILE%\Desktop\esc1"
+mkdir "%USERPROFILE%\Desktop\esc2"
+mkdir "%USERPROFILE%\Desktop\esc3"
+mkdir "%USERPROFILE%\Desktop\esc4"
+mkdir "%USERPROFILE%\Desktop\esc5"
+mkdir "%USERPROFILE%\Desktop\esc6"
+mkdir "%USERPROFILE%\Desktop\esc7"
+)
+
 exit
